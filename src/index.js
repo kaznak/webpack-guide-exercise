56161c69ef5e3c5416ed436e87a883da3afa5bd5
Kazutaka <kaznak.at.work@gmail.com>
2019-06-19 10:00:40 +0900

    Revert  following
    1. "add asset example"
       - 03a5e82e572c1c37bd3d76840be162ea41fed699.
    2. "update yarn.lock"
       - 0c4dbe995df04d4e57d277a37f6e5ef70fcfee29.
    3. "add loaders"
       - d5becd5929d4cefabeb10400ff1d4fe97bb48379.
    4. "add loaders"
       - f674e6959e095910d753ba2821edd016185e2ca6.
    5. "update .gitignore"
       - 492a1e3a45db543230e10412446129aa7899238e.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    /* Add the image to our existing div. */
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
